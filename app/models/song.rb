4df817aa2d85c5ca1af26ac8dcc3710d969c618b
Everett <everett.bowline@gmail.com>
2018-10-07 20:41:51 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,13 +2,13 @@ class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: { scope: [:artist_name, :release_year], message: "The title cannot be repeated by the same artist in the same year." }
   validates :released, inclusion: { in: [true, false] }
 
-  with_options if: :is_the_song_released? do
+  with_options if: :released? do
     validates :release_year, presence: true
   end
 
   validates :artist_name, presence: true
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
-  def is_the_song_released?
+  def released?
     released
   end
 end
