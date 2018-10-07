ff2503834129bfb81a5eb693ae7862df3cb32626
Everett <everett.bowline@gmail.com>
2018-10-07 20:43:41 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,7 @@ class Song < ActiveRecord::Base
     validates :release_year, presence: true
     validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
   end
-  
+
   def released?
     released
   end
